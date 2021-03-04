62d550b94c15b4214c8a3d41cc0b946aa22cecf6
Masato516 <yaginen9@gmail.com>
2021-03-04 10:12:01 +0000

    add button template

--- src/line.rb
+++ src/line.rb
@@ -2,6 +2,44 @@
 require 'sinatra'
 require 'line/bot'
 
+def template
+  {
+    "type": "template",
+    "altText": "This is a buttons template",
+    "template": {
+        "type": "buttons",
+        "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+        "imageAspectRatio": "rectangle",
+        "imageSize": "cover",
+        "imageBackgroundColor": "#FFFFFF",
+        "title": "Menu",
+        "text": "Please select",
+        "defaultAction": {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+        },
+        "actions": [
+            {
+              "type": "postback",
+              "label": "Buy",
+              "data": "action=buy&itemid=123"
+            },
+            {
+              "type": "postback",
+              "label": "Add to cart",
+              "data": "action=add&itemid=123"
+            },
+            {
+              "type": "uri",
+              "label": "View detail",
+              "uri": "http://example.com/page/123"
+            }
+        ]
+    }
+  }
+end
+
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_id = ENV["LINE_CHANNEL_ID"]
@@ -31,14 +69,18 @@ post '/callback' do
 
         if event.message['text'] =~ /おみくじ/
           message[:text] = %w(大吉 中吉 小吉 凶 大凶).shuffle.first
+          client.reply_message(event['replyToken'], message)
+        elsif event.message['text'] =~ /駅/
+          client.reply_message(event['replyToken'], template)
         end
+
         
-        client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
         tf.write(response.body)
       end
+    when
     end
   end
 
